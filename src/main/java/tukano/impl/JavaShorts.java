a559e74f832d2b2dd22bee8c9238881634517ef9
José Trigueiro <joseleonardo2000@gmail.com>
2024-10-23 18:55:52 +0100

    lab3 beginning
    Co-Authored-By: Diogo Lemos <145502790+dblemos@users.noreply.github.com>

--- src/main/java/tukano/impl/JavaShorts.java
+++ src/main/java/tukano/impl/JavaShorts.java
@@ -23,30 +23,31 @@ import tukano.impl.data.Following;
 import tukano.impl.data.Likes;
 import tukano.impl.rest.TukanoRestServer;
 import utils.DB;
+import tukano.impl.storage.CosmosDBLayer;
 
 public class JavaShorts implements Shorts {
 
 	private static Logger Log = Logger.getLogger(JavaShorts.class.getName());
-	
+
 	private static Shorts instance;
-	
+
 	synchronized public static Shorts getInstance() {
-		if( instance == null )
+		if (instance == null)
 			instance = new JavaShorts();
 		return instance;
 	}
-	
-	private JavaShorts() {}
-	
-	
+
+	private JavaShorts() {
+	}
+
 	@Override
 	public Result<Short> createShort(String userId, String password) {
 		Log.info(() -> format("createShort : userId = %s, pwd = %s\n", userId, password));
 
-		return errorOrResult( okUser(userId, password), user -> {
-			
+		return errorOrResult(okUser(userId, password), user -> {
+
 			var shortId = format("%s+%s", userId, UUID.randomUUID());
-			var blobUrl = format("%s/%s/%s", TukanoRestServer.serverURI, Blobs.NAME, shortId); 
+			var blobUrl = format("%s/%s/%s", TukanoRestServer.serverURI, Blobs.NAME, shortId);
 			var shrt = new Short(shortId, userId, blobUrl);
 
 			return errorOrValue(DB.insertOne(shrt), s -> s.copyWithLikes_And_Token(0));
@@ -57,32 +58,31 @@ public class JavaShorts implements Shorts {
 	public Result<Short> getShort(String shortId) {
 		Log.info(() -> format("getShort : shortId = %s\n", shortId));
 
-		if( shortId == null )
+		if (shortId == null)
 			return error(BAD_REQUEST);
 
 		var query = format("SELECT count(*) FROM Likes l WHERE l.shortId = '%s'", shortId);
 		var likes = DB.sql(query, Long.class);
-		return errorOrValue( getOne(shortId, Short.class), shrt -> shrt.copyWithLikes_And_Token( likes.get(0)));
+		return errorOrValue(getOne(shortId, Short.class), shrt -> shrt.copyWithLikes_And_Token(likes.get(0)));
 	}
 
-	
 	@Override
 	public Result<Void> deleteShort(String shortId, String password) {
 		Log.info(() -> format("deleteShort : shortId = %s, pwd = %s\n", shortId, password));
-		
-		return errorOrResult( getShort(shortId), shrt -> {
-			
-			return errorOrResult( okUser( shrt.getOwnerId(), password), user -> {
-				return DB.transaction( hibernate -> {
 
-					hibernate.remove( shrt);
-					
+		return errorOrResult(getShort(shortId), shrt -> {
+
+			return errorOrResult(okUser(shrt.getOwnerId(), password), user -> {
+				return DB.transaction(hibernate -> {
+
+					hibernate.remove(shrt);
+
 					var query = format("DELETE Likes l WHERE l.shortId = '%s'", shortId);
-					hibernate.createNativeQuery( query, Likes.class).executeUpdate();
-					
-					JavaBlobs.getInstance().delete(shrt.getBlobUrl(), Token.get() );
+					hibernate.createNativeQuery(query, Likes.class).executeUpdate();
+
+					JavaBlobs.getInstance().delete(shrt.getBlobUrl(), Token.get());
 				});
-			});	
+			});
 		});
 	}
 
@@ -91,36 +91,36 @@ public class JavaShorts implements Shorts {
 		Log.info(() -> format("getShorts : userId = %s\n", userId));
 
 		var query = format("SELECT s.shortId FROM Short s WHERE s.ownerId = '%s'", userId);
-		return errorOrValue( okUser(userId), DB.sql( query, String.class));
+		return errorOrValue(okUser(userId), DB.sql(query, String.class));
 	}
 
 	@Override
 	public Result<Void> follow(String userId1, String userId2, boolean isFollowing, String password) {
-		Log.info(() -> format("follow : userId1 = %s, userId2 = %s, isFollowing = %s, pwd = %s\n", userId1, userId2, isFollowing, password));
-	
-		
-		return errorOrResult( okUser(userId1, password), user -> {
+		Log.info(() -> format("follow : userId1 = %s, userId2 = %s, isFollowing = %s, pwd = %s\n", userId1, userId2,
+				isFollowing, password));
+
+		return errorOrResult(okUser(userId1, password), user -> {
 			var f = new Following(userId1, userId2);
-			return errorOrVoid( okUser( userId2), isFollowing ? DB.insertOne( f ) : DB.deleteOne( f ));	
-		});			
+			return errorOrVoid(okUser(userId2), isFollowing ? DB.insertOne(f) : DB.deleteOne(f));
+		});
 	}
 
 	@Override
 	public Result<List<String>> followers(String userId, String password) {
 		Log.info(() -> format("followers : userId = %s, pwd = %s\n", userId, password));
 
-		var query = format("SELECT f.follower FROM Following f WHERE f.followee = '%s'", userId);		
-		return errorOrValue( okUser(userId, password), DB.sql(query, String.class));
+		var query = format("SELECT f.follower FROM Following f WHERE f.followee = '%s'", userId);
+		return errorOrValue(okUser(userId, password), DB.sql(query, String.class));
 	}
 
 	@Override
 	public Result<Void> like(String shortId, String userId, boolean isLiked, String password) {
-		Log.info(() -> format("like : shortId = %s, userId = %s, isLiked = %s, pwd = %s\n", shortId, userId, isLiked, password));
+		Log.info(() -> format("like : shortId = %s, userId = %s, isLiked = %s, pwd = %s\n", shortId, userId, isLiked,
+				password));
 
-		
-		return errorOrResult( getShort(shortId), shrt -> {
+		return errorOrResult(getShort(shortId), shrt -> {
 			var l = new Likes(userId, shortId, shrt.getOwnerId());
-			return errorOrVoid( okUser( userId, password), isLiked ? DB.insertOne( l ) : DB.deleteOne( l ));	
+			return errorOrVoid(okUser(userId, password), isLiked ? DB.insertOne(l) : DB.deleteOne(l));
 		});
 	}
 
@@ -128,11 +128,11 @@ public class JavaShorts implements Shorts {
 	public Result<List<String>> likes(String shortId, String password) {
 		Log.info(() -> format("likes : shortId = %s, pwd = %s\n", shortId, password));
 
-		return errorOrResult( getShort(shortId), shrt -> {
-			
-			var query = format("SELECT l.userId FROM Likes l WHERE l.shortId = '%s'", shortId);					
-			
-			return errorOrValue( okUser( shrt.getOwnerId(), password ), DB.sql(query, String.class));
+		return errorOrResult(getShort(shortId), shrt -> {
+
+			var query = format("SELECT l.userId FROM Likes l WHERE l.shortId = '%s'", shortId);
+
+			return errorOrValue(okUser(shrt.getOwnerId(), password), DB.sql(query, String.class));
 		});
 	}
 
@@ -141,50 +141,50 @@ public class JavaShorts implements Shorts {
 		Log.info(() -> format("getFeed : userId = %s, pwd = %s\n", userId, password));
 
 		final var QUERY_FMT = """
-				SELECT s.shortId, s.timestamp FROM Short s WHERE	s.ownerId = '%s'				
-				UNION			
-				SELECT s.shortId, s.timestamp FROM Short s, Following f 
-					WHERE 
-						f.followee = s.ownerId AND f.follower = '%s' 
+				SELECT s.shortId, s.timestamp FROM Short s WHERE	s.ownerId = '%s'
+				UNION
+				SELECT s.shortId, s.timestamp FROM Short s, Following f
+					WHERE
+						f.followee = s.ownerId AND f.follower = '%s'
 				ORDER BY s.timestamp DESC""";
 
-		return errorOrValue( okUser( userId, password), DB.sql( format(QUERY_FMT, userId, userId), String.class));		
+		return errorOrValue(okUser(userId, password), DB.sql(format(QUERY_FMT, userId, userId), String.class));
 	}
-		
-	protected Result<User> okUser( String userId, String pwd) {
+
+	protected Result<User> okUser(String userId, String pwd) {
 		return JavaUsers.getInstance().getUser(userId, pwd);
 	}
-	
-	private Result<Void> okUser( String userId ) {
-		var res = okUser( userId, "");
-		if( res.error() == FORBIDDEN )
+
+	private Result<Void> okUser(String userId) {
+		var res = okUser(userId, "");
+		if (res.error() == FORBIDDEN)
 			return ok();
 		else
-			return error( res.error() );
+			return error(res.error());
 	}
-	
+
 	@Override
 	public Result<Void> deleteAllShorts(String userId, String password, String token) {
 		Log.info(() -> format("deleteAllShorts : userId = %s, password = %s, token = %s\n", userId, password, token));
 
-		if( ! Token.isValid( token, userId ) )
+		if (!Token.isValid(token, userId))
 			return error(FORBIDDEN);
-		
-		return DB.transaction( (hibernate) -> {
-						
-			//delete shorts
-			var query1 = format("DELETE Short s WHERE s.ownerId = '%s'", userId);		
+
+		return DB.transaction((hibernate) -> {
+
+			// delete shorts
+			var query1 = format("DELETE Short s WHERE s.ownerId = '%s'", userId);
 			hibernate.createQuery(query1, Short.class).executeUpdate();
-			
-			//delete follows
-			var query2 = format("DELETE Following f WHERE f.follower = '%s' OR f.followee = '%s'", userId, userId);		
+
+			// delete follows
+			var query2 = format("DELETE Following f WHERE f.follower = '%s' OR f.followee = '%s'", userId, userId);
 			hibernate.createQuery(query2, Following.class).executeUpdate();
-			
-			//delete likes
-			var query3 = format("DELETE Likes l WHERE l.ownerId = '%s' OR l.userId = '%s'", userId, userId);		
+
+			// delete likes
+			var query3 = format("DELETE Likes l WHERE l.ownerId = '%s' OR l.userId = '%s'", userId, userId);
 			hibernate.createQuery(query3, Likes.class).executeUpdate();
-			
+
 		});
 	}
-	
+
 }
---
+++ src/main/java/tukano/impl/storage/CosmosDBLayer.java
@@ -0,0 +1,112 @@
+package tukano.impl.storage;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+import com.azure.cosmos.ConsistencyLevel;
+import com.azure.cosmos.CosmosClient;
+import com.azure.cosmos.CosmosClientBuilder;
+import com.azure.cosmos.CosmosContainer;
+import com.azure.cosmos.CosmosDatabase;
+import com.azure.cosmos.CosmosException;
+import com.azure.cosmos.models.CosmosItemRequestOptions;
+import com.azure.cosmos.models.CosmosQueryRequestOptions;
+import com.azure.cosmos.models.PartitionKey;
+
+import tukano.api.Result;
+
+import static tukano.api.Result.ErrorCode;
+
+public class CosmosDBLayer {
+	private static final String CONNECTION_URL = "https://por preencher"; // TODO
+	private static final String DB_KEY = "por preencher=="; // TODO
+	private static final String DB_NAME = "por preencher"; // TODO
+	// private static final String CONTAINER = "users";
+
+	private static CosmosDBLayer instance;
+
+	private String containerName;
+
+	public static synchronized CosmosDBLayer getInstance(String containerName) {
+		if (instance != null)
+			return instance;
+
+		CosmosClient client = new CosmosClientBuilder()
+				.endpoint(CONNECTION_URL)
+				.key(DB_KEY)
+				.directMode()
+				// .gatewayMode() // replace by .directMode() for better performance
+				.consistencyLevel(ConsistencyLevel.BOUNDED_STALENESS)
+				.connectionSharingAcrossClientsEnabled(true)
+				.contentResponseOnWriteEnabled(true) // On write, return the object written
+				.buildClient();
+		instance = new CosmosDBLayer(client, containerName);
+		return instance;
+
+	}
+
+	private CosmosClient client;
+	private CosmosDatabase db;
+	private CosmosContainer container;
+
+	public CosmosDBLayer(CosmosClient client, String containerName) {
+		this.client = client;
+		this.containerName = containerName;
+	}
+
+	private synchronized void init() {
+		if (db != null)
+			return;
+		db = client.getDatabase(DB_NAME);
+		container = db.getContainer(containerName);
+	}
+
+	public void close() {
+		client.close();
+	}
+
+	public <T> Result<T> getOne(String id, Class<T> clazz) {
+		return tryCatch(() -> container.readItem(id, new PartitionKey(id), clazz).getItem());
+	}
+
+	public <T> Result<?> deleteOne(T obj) {
+		return tryCatch(() -> container.deleteItem(obj, new CosmosItemRequestOptions()).getItem());
+	}
+
+	public <T> Result<T> updateOne(T obj) {
+		return tryCatch(() -> container.upsertItem(obj).getItem());
+	}
+
+	public <T> Result<T> insertOne(T obj) {
+		return tryCatch(() -> container.createItem(obj).getItem());
+	}
+
+	public <T> Result<List<T>> query(Class<T> clazz, String queryStr) {
+		return tryCatch(() -> {
+			var res = container.queryItems(queryStr, new CosmosQueryRequestOptions(), clazz);
+			return res.stream().toList();
+		});
+	}
+
+	<T> Result<T> tryCatch(Supplier<T> supplierFunc) {
+		try {
+			init();
+			return Result.ok(supplierFunc.get());
+		} catch (CosmosException ce) {
+			// ce.printStackTrace();
+			return Result.error(errorCodeFromStatus(ce.getStatusCode()));
+		} catch (Exception x) {
+			x.printStackTrace();
+			return Result.error(ErrorCode.INTERNAL_ERROR);
+		}
+	}
+
+	static Result.ErrorCode errorCodeFromStatus(int status) {
+		return switch (status) {
+			case 200 -> ErrorCode.OK;
+			case 404 -> ErrorCode.NOT_FOUND;
+			case 409 -> ErrorCode.CONFLICT;
+			default -> ErrorCode.INTERNAL_ERROR;
+		};
+	}
+}
